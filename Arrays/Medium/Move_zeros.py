219695d060b643d638ebc4d51d02c92433531481
Rashel Akhtar Ahmed <akhtarrashel408@gmail.com>
2026-01-12 20:08:19 +0530

    Solve move zero prblm

--- Arrays/Medium/Move_zeros.py
+++ Arrays/Medium/Move_zeros.py
@@ -1,15 +1,33 @@
 # Move all zeros to the end of the array.
 
 arr = [1, 0, 2, 3, 0, 7]
+temp = []
 count_zero = 0
 
 for i in arr:
-    if i == 0:
-        arr.pop(i)
-        print(f"Array: {arr}")
+    if i != 0:
+        temp.append(i)
+    else:
         count_zero += 1
-    
-for i in range(count_zero):
-    arr.append(0)
 
-print(arr)
+for i in range(count_zero):
+    temp.append(0)
+
+print(temp)
+
+
+# ------------------------------------------------------------------------------------------------------ #
+
+# Sol. with better space complexity
+
+arr2 = [1, 0, 2, 3, 0, 7, 0, 1]
+
+pos = 0
+
+for i in range(len(arr2)):
+    if arr2[i] != 0:
+        arr2[pos], arr2[i] = arr2[i], arr2[pos]
+
+        pos += 1
+
+print(arr2)
