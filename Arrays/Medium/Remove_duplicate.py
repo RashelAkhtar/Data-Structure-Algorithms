c1bd0b42d9350e83fb32f3b9bba424f093a7bad7
Rashel Akhtar Ahmed <akhtarrashel408@gmail.com>
2026-01-13 20:57:08 +0530

    Solve Remove duplicate

--- Arrays/Medium/Remove_duplicate.py
+++ Arrays/Medium/Remove_duplicate.py
@@ -1,2 +1,11 @@
 # Remove duplicate elements from a sorted array.
 
+arr = [1,1,2,2,3, 4, 5, 5, 9, 9, 11, 12, 13, 13]
+slow = 0
+
+for fast in range(len(arr)):
+    if arr[fast] != arr[slow]:
+        slow += 1
+        arr[fast], arr[slow] = arr[slow], arr[fast]
+
+print(arr[:slow])
